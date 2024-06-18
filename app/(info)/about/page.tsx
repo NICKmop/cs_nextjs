af6c83d516ef546605d13109e0dced67326d0fde
NICKmop <dlckdtjq153@gmail.com>
2024-06-18 18:00:59 +0900

    commit

--- app/(info)/about/page.tsx
+++ app/(info)/about/page.tsx
@@ -1,7 +1,9 @@
+import styles from './style/about.module.css';
+import { KEEPGOING_IMG } from '../../constant';
+import Link from 'next/link';
+
 export async function getMyinfo() {
-    console.log("befroe promise Time");
     await new Promise((resolve) => setTimeout(resolve, 5000));
-    console.log("after promise Time");
     return ;
 }
 
@@ -9,8 +11,21 @@ export default function About(){
     let info = getMyinfo();
 
     return(
-        <div>
-            About me
+        <div className={styles.main} style={{backgroundImage: `url('${KEEPGOING_IMG}')`}}>
+            <div className={styles.header}>
+                <ul>
+                    <Link href='/'>subject_1</Link>
+                    <Link href='#'>subject_2</Link>
+                    <Link href='#'>subject_3</Link>
+                    <Link href='/about'>About</Link>
+                </ul>
+            </div>
+            <div className={styles.content}>
+                CONTENT
+            </div>
+            <div className={styles.footer}>
+                FOOTER
+            </div>
         </div>
     )
 }
--- app/constant.ts
+++ app/constant.ts
@@ -2,5 +2,6 @@ const FALL_IMG = "/img/fall.jpg";
 const SPRING_IMG = "/img/spring.jpg";
 const SUMMER_IMG = "/img/summer.jpg";
 const WINTER_IMG = "/img/winter.jpg";
+const KEEPGOING_IMG = "/img/keep-going.jpg";
 
-export {FALL_IMG, SPRING_IMG, SUMMER_IMG, WINTER_IMG}
+export {FALL_IMG, SPRING_IMG, SUMMER_IMG, WINTER_IMG, KEEPGOING_IMG}
